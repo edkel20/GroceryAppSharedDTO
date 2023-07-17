f4abd18763d5324d0278f695df5c430ff6539a8f
edkel20 <ed.kel20@gmail.com>
2023-07-17 19:55:05 +0200

    added GroceryItemRequestDTO and GroceryItemResponseDTO

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eden NKOUKA on 17/07/2023.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eden NKOUKA on 17/07/2023.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO: Codable {
+    public let id: UUID
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(id: UUID, title: String, price: Double, quantity: Int) throws {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+        self.id = id
+    }
+}
